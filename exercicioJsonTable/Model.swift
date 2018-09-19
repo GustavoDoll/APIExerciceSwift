89f70f16b2067b845197cb5cddb37197b9b2f7e0
Usuário Convidado <Guest@PA06MICRO11.local>
2018-09-19 09:21:42 -0300

    Json,lista de melhores apps free na appstore

---
+++ exercicioJsonTable/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  exercicioJsonTable
+//
+//  Created by Usuário Convidado on 19/09/2018.
+//  Copyright © 2018 Usuário Convidado. All rights reserved.
+//
+
+import UIKit
+
+class Model: NSObject {
+
+}
